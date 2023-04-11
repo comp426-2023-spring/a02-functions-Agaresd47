c10d092843f0131c395de79795bb1c8a24108fe4
Agaresd47 <zdong@unc.edu>
2023-04-11 14:29:22 -0400

    near finish

--- cli.js
+++ cli.js
@@ -4,7 +4,7 @@ import moment from "moment-timezone";
 import fetch from "node-fetch";
 import minimist from "minimist";
 
-const args = minimist(process.argv.slice(2))
+const args = minimist(process.argv.slice(2));
 
 if (args.h){
     console.log(`Usage: galosh.js [options] -[n|s] LATITUDE -[e|w] LONGITUDE -z TIME_ZONE
@@ -49,7 +49,7 @@ if (args.t) {
 }
 
 if ("d" in args) {
-    days = args.d 
+    days = args.d; 
 } else {
     days = 1;
 }
@@ -60,7 +60,7 @@ const data = await response.json();
 
 if(args.j) {
     console.log(data);
-    process.exit(0)
+    process.exit(0);
 }
 
 
@@ -69,6 +69,8 @@ if(data.daily.precipitation_hours[days] > 0) {
     string = "It's raining ";
 } else {
     string = "It's sunny ";}
+
+    
 if (days == 0) {
     string += "today.";
 } else if (days > 1) {
@@ -80,3 +82,4 @@ if (days == 0) {
 
 
 console.log(string); 
+process.exit(0);
